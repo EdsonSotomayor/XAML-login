15791eb0620da9174ab9c41ad3dd170353c46b97
Alumno <Alumno@PC04>
2019-09-18 12:30:39 -0500

    v1

---
+++ Login/Login/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Login con puerta abierta";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void Btiniciar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtnombre.Text=="Juancho" && txtcontra.Text=="123tamarindo")
+            {
+                Window1 form = new Window1();
+                form.ShowDialog();
+            }
+        }
+    }
+}
